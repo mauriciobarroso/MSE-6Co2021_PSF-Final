70cbf5a735084804aa866c6633bd3fc11376e227
Mauricio Barroso Benavides <mauriciobarroso1990@gmail.com>
2021-09-11 21:51:12 -0400

    Refactor code

--- src/main.c
+++ src/main.c
@@ -13,11 +13,16 @@
 #include "fir.h"
 
 /* macros --------------------------------------------------------------------*/
-#define ADC_BITS		10		/**< Number of ADC bits */
-#define SAMPLING_FREQ	10000	/**< Sampling frequency in Hz */
-#define SAMPLES_NUM		H_PADD_LENGTH - h_LENGTH + 1	/**< Number of samples */
-#define CUT_FREQ_1		1700	/**< Sampling frequency in Hz */
-#define CUT_FREQ_2		2300	/**< Sampling frequency in Hz */
+#define FFT_LENGTH			512		/**< Number of ADC bits */
+#define ADC_BITS			10		/**< Number of ADC bits */
+#define SAMPLING_FREQ		10000	/**< Sampling frequency in Hz */
+#define SAMPLES_NUM			118		/**< Number of samples */
+#define CUT_FREQ_1			1700	/**< Sampling frequency in Hz */
+#define CUT_FREQ_2			2300	/**< Sampling frequency in Hz */
+
+/* */
+#define	CONCATENATE(X)		arm_cfft_sR_q15_len ## X
+#define FFT_INIT(X)			CONCATENATE(X)
 
 /* typedef -------------------------------------------------------------------*/
 
@@ -35,6 +40,14 @@ typedef struct {
 	char pos[4];	/**< */
 } __attribute__ ((packed)) packet_t;
 
+typedef struct {
+	uint16_t cutBinFreq1;
+	uint16_t cutBinFreq2;
+	uint16_t counter;
+	uint16_t acum;
+	uint8_t zeros;
+} app_t;
+
 /* data declaration ----------------------------------------------------------*/
 
 packet_t packet = {
@@ -49,33 +62,44 @@ packet_t packet = {
 		.pos = "end*"
 };
 
+app_t app = {
+		.cutBinFreq1 = CUT_FREQ_1 / (SAMPLING_FREQ / FFT_LENGTH),
+		.cutBinFreq2 = CUT_FREQ_2 / (SAMPLING_FREQ / FFT_LENGTH),
+		.counter = 0,
+		.acum = 0,
+		.zeros = 0
+};
+
 /* function declaration ------------------------------------------------------*/
+
+void process(app_t * data, q15_t * array);
 
 /* main ----------------------------------------------------------------------*/
 
 void main(void) {
+	/* Buffers */
+	int16_t adc[FFT_LENGTH];
+	q15_t fft[FFT_LENGTH * 2];
+	q15_t fftAbs[FFT_LENGTH];
+
+	/**/
 	uint16_t sample = 0;
-	int16_t adc[packet.n + packet.m - 1];
-	q15_t fftInOut[(packet.n + packet.m - 1) * 2];
-	q15_t fftAbs[packet.n + packet.m - 1];
+//	uint16_t zeros = 0;
+//	uint16_t acum = 0;
+//
+	uint16_t cutBinInit = CUT_FREQ_1 / (SAMPLING_FREQ / FFT_LENGTH);
+	uint16_t cutBinFinal= CUT_FREQ_2 / (SAMPLING_FREQ / FFT_LENGTH);
 
-	int cutBinInit = CUT_FREQ_1 / (packet.fs / (packet.n + packet.m - 1));
-	int cutBinFinal= CUT_FREQ_2 / (packet.fs / (packet.n + packet.m - 1));
-	uint16_t counter = cutBinFinal;
-	uint16_t iter = 0;
-	uint16_t acum = 0;
-
-	for(; sample < packet.n + packet.m - 1; sample++) {
+	for(; sample < FFT_LENGTH; sample++) {
 		adc[sample] = 0;
-		fftInOut[sample * 2] = 0;
-		fftInOut[(sample * 2) + 1 ] = 0;
+		fft[sample * 2] = 0;
+		fft[(sample * 2) + 1 ] = 0;
 	}
 
 	/* Initialize board peripherals */
 	boardConfig();
 	uartConfig(UART_USB, 460800);
 	adcConfig(ADC_ENABLE);
-//	dacConfig(DAC_ENABLE);
 	cyclesCounterInit(EDU_CIAA_NXP_CLOCK_SPEED);
 
 	/* Infinite loop */
@@ -87,102 +111,82 @@ void main(void) {
 		if(sample >= packet.n) {
 			gpioToggle(LEDR);
 
-			/*  */
-			arm_cfft_q15(&arm_cfft_sR_q15_len512, fftInOut, 0, 1);
-			arm_cmplx_mag_squared_q15(fftInOut, fftAbs, packet.n + packet.m - 1);
-			arm_mult_q15(fftAbs, HAbs, fftAbs, packet.n + packet.m - 1);
+			/* Process signal FFT with filter */
+			arm_cfft_q15(&FFT_INIT(FFT_LENGTH), fft, 0, 1);
+			arm_cmplx_mag_squared_q15(fft, fftAbs, FFT_LENGTH);
+			arm_mult_q15(fftAbs, HAbs, fftAbs, FFT_LENGTH);
 
-//			for(uint16_t i = 0; i < (packet.n + packet.m - 1) / 2; i++) {
-//				if(i >= cutBinInit && i <= cutBinFinal) {
-//					if(fftAbs[i] > 1) {
-//						gpioToggle(LED2);
-//					}
-//				}
-//			}
+			/* Look for sounds into CUT_FREQ_1 and CUT_FREQ_2 frequencies, and toggle LED */
+			process(&app, fftAbs);
 
-//			while(counter >= cutBinInit) {
-////				iter++;
-//
-//				if(fftAbs[counter] > 0) {
-//					counter--;
-//
-//					packet.counter++;
-//
-//					if(packet.counter++ >= 5) {
-//						gpioToggle(LED2);
-//						counter = cutBinFinal;
-//						break;
-//					}
-//				}
-//				else {
-//					break;
-//				}
-//
-//			}
-
-
-//			if(iter == 20) {
-//				counter = cutBinFinal;
-//				iter = 0;
-//			}
-
-
-			for(uint16_t i = cutBinFinal; i >= cutBinInit; i--) {
-				if(fftAbs[i] > 1) {
-					packet.counter++;
-				}
-			}
-
-			if(packet.counter == 0) {
-				iter++;
-
-				if(iter == 2) {
-					acum = 0;
-					iter = 0;
-				}
-			}
-
-			acum += packet.counter;
-
-			if(acum  > 45) {
-				gpioToggle(LED2);
-				acum = 0;
-			}
-
-			/**/
+			/* Add id and send packet */
 			packet.id++;
 			uartWriteByteArray(UART_USB, (uint8_t *)&packet, sizeof(packet_t));
 
-			/* Send ADC and DFT data */
-			for(sample = 0; sample < packet.n + packet.m - 1; sample++) {
+			/* Send ADC and FFT data */
+			for(sample = 0; sample < FFT_LENGTH; sample++) {
 				uartWriteByteArray(UART_USB, (uint8_t *)&adc[sample], sizeof(* adc));
 				uartWriteByteArray(UART_USB, (uint8_t *)&fftAbs[sample], sizeof(* fftAbs));
 			}
 
-			/* Clear ... */
-			packet.counter = 0;
+			/* Clear samples and read ADC */
 			sample = 0;
 			adcRead(CH1);
 		}
 
-		/* Get ADC sample */
+		/* Get ADC sample and rescale */
 		adc[sample] = (((int16_t)adcRead(CH1) - 512) >> (10 - ADC_BITS)) << (6 + 10 - ADC_BITS); /* PISA el sample que se acaba de mandar con una nueva muestra */
 		adc[sample] *= 10;
+
 		/* Write ADC samples in even positions and 0 in odd positions of the FFT array */
-		fftInOut[sample * 2] = adc[sample];
-		fftInOut[(sample * 2) + 1] = 0;
+		fft[sample * 2] = adc[sample];
+		fft[(sample * 2) + 1] = 0;
 
 		sample++;
 
 
 
 		/* Toggle LED1 and wait while the cycles counter is less than
-		 * EDU_CIAA_NXP_CLOCK_SPEED/packet.fs * 1000. EDU_CIAA_NXP_CLOCK_SPEED is 204000000 */
+		 * EDU_CIAA_NXP_CLOCK_SPEED/packet.fs. EDU_CIAA_NXP_CLOCK_SPEED is 204000000 */
 		gpioToggle(LED1); /* este led blinkea a fs/2 */
 		while(cyclesCounterRead() < EDU_CIAA_NXP_CLOCK_SPEED/packet.fs);
 	}
 }
 
 /* function definition -------------------------------------------------------*/
+
+void process(app_t * data, q15_t * array) {
+	/* Search in the bandwith the frequencies for non-zero values */
+	for(uint16_t i = data->cutBinFreq2; i >= data->cutBinFreq1; i--) {
+		if(array[i] > 1) {
+			data->counter++;
+		}
+	}
+
+	/* If two zeros are found, then reset the variables  */
+	if(data->counter == 0) {
+		data->zeros++;
+
+		if(data->zeros == 2) {
+			data->acum = 0;
+			data->zeros = 0;
+		}
+	}
+	else {
+		data->zeros = 0;
+	}
+
+	/* Increment the acumulator and if the values is greather
+	 * than 45, then toggle the LED */
+	data->acum += data->counter;
+
+	if(data->acum > 45) {
+		gpioToggle(LED2);
+		data->acum = 0;
+	}
+
+	/* Reset the variable */
+	data->counter = 0;
+}
 
 /* end of file ---------------------------------------------------------------*/
